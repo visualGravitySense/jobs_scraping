6040715db4c886a5fc92b364de4793520e2a9c3c
visualGravitySense <helikeel@gmail.com>
2021-01-25 17:02:26 +0200

    Django development process

---
+++ scraping/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+
+from scraping.models import City, Language
+
+
+class FindForm(forms.Form):
+    city = forms.ModelChoiceField(queryset=City.objects.all(), to_field_name="slug", required=False,
+        widget=forms.Select(attrs={'class': 'form-control'}),
+                                  label='City'
+    )
+    language = forms.ModelChoiceField(queryset=Language.objects.all(), to_field_name="slug", required=False,
+                                      widget=forms.Select(attrs={'class': 'form-control'}),
+                                      label='Specialty'
+    )
+    #name = forms.CharField(initial='class')
--- scraping/views.py
+++ scraping/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
 from .models import Vacancy
+from .forms import FindForm
 
 # Create your views here.
 def home_view(request):
-    qs = Vacancy.objects.all()
-    return render(request, 'scraping/home.html', {'object_list': qs})
+    #print(request.GET)
+    form = FindForm()
+    city = request.GET.get('city')
+    language = request.GET.get('language')
+    qs = []
+    if city or language:
+        _filter = {}
+        if city:
+            _filter['city__slug'] = city
+        if language:
+            _filter['language__slug'] = language
+
+        qs = Vacancy.objects.filter(**_filter)
+    return render(request, 'scraping/home.html', {'object_list': qs,
+                                                  'form': form})
